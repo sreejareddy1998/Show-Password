8a759e0039a32d58154b39619720ee3220f3d3ab
sreejareddy1998 <51157924+sreejareddy1998@users.noreply.github.com>
2023-07-18 16:56:36 +0530

    modified code using ternery operator

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ import './style.css';
 // Write Javascript code!
 window.myFunction = function () {
   let x = document.getElementById('password');
-  if (x.type === 'password') {
-    x.type = 'text';
-  } else {
-    x.type = 'password';
-  }
+  x.type = x.type === 'password' ? 'text' : 'password';
+  // if (x.type === 'password') {
+  //   x.type = 'text';
+  // } else {
+  //   x.type = 'password';
+  // }
   console.log(x.type);
 };
